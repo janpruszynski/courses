4599b99c7cf78fe32e92aa70bde22689cfb06a91
janpruszynski <janpruszynski@outlook.com>
2019-09-22 15:33:38 +0200

    added mock API server

--- client/src/redux/actions/courseActions.js
+++ client/src/redux/actions/courseActions.js
@@ -1,5 +1,23 @@
 import * as courseActionTypes from "../constants/actionConstants";
+import * as courseApi from "../../api/courseApi";
 
 export function createCourse(course) {
   return { type: courseActionTypes.CREATE_COURSE, course };
+}
+
+export function loadCourseSuccess(courses) {
+  return { type: courseActionTypes.LOAD_COURSES_SUCCESS, courses };
+}
+
+export function loadCourses() {
+  return function(dispatch) {
+    return courseApi
+      .getCourses()
+      .then(courses => {
+        dispatch(loadCourseSuccess(courses));
+      })
+      .catch(error => {
+        throw error;
+      }); //dispatch(loadCoursesError(error)));
+  };
 }
--- client/src/redux/reducers/courseReducer.js
+++ client/src/redux/reducers/courseReducer.js
@@ -4,6 +4,8 @@ export default function courseReducer(state = [], action) {
   switch (action.type) {
     case courseActionTypes.CREATE_COURSE:
       return [...state, { ...action.course }];
+    case courseActionTypes.LOAD_COURSES_SUCCESS:
+      return action.courses;
     default:
       return state;
   }
